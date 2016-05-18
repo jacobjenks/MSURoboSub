1715af4826f2036b7b4185e40de70fa5690f040c
Jake <jacobjenks@gmail.com>
2016-05-18 17:33:21 -0600

    Worked on integrating rosserial_arduino. Mostly just need to figure out message types for pubs/subs.

--- arduino/sensorMotorControl/sensorMotorControl.ino
+++ arduino/sensorMotorControl/sensorMotorControl.ino
@@ -1,10 +1,22 @@
 #include <Wire.h>
 #include "Arduino_I2C_ESC.h"
 #include <ctype.h>
+#include <ros.h>
+
 
 int signal;
 const int numMotors = 6;
 String serialMessage;
+
+ros::NodeHandle nh;
+std_msgs:: motorStatusMsg;
+ros::Publisher pubMotorStatus("motor_status", &motorStatusMsg);
+std_msgs:: motorRPMMsg;
+ros::Publisher pubMotorRpm("motor_rpm", &motorRPMMsgs);
+std_msgs::Float32 depthMsg;
+ros::Publisher pubDepth("depth", &depthMsg);
+std_msgs:: hydroMsg;
+ros::Publisher pubHydro("hydrophone", &hdryMsg)
 
 enum MotorE{
   MEForwardPort,
@@ -29,12 +41,20 @@ int motorRPM[numMotors];//Motor RPM
 //Array of % thrust values we'd like each motor to operate at
 //Values range between -1 and 1 for 100% forward/reverse thrust
 float motorCommand[] = {0,0,0,0,0,0};
+float maxThrust = .75;//Percent value indicating what power level we consider to be max thrust
+
+void motorCommandCallback(const std_msgs::Empty& command){
+//route command to motor
+//msg type needs to be something other than Empty, but not sure what it should be yet
+//msg should contain motor ID and thrust setting
+}
+
+ros::Subscriber<std_msgs::Empty> commandSub("motorCommand", &motorCommandCB);
 
 void setup() {
-  Serial.begin(57600);
-  Serial.println("Starting");
-  
   Wire.begin();
+  nh.initNode();
+  nh.advertise(depth);
 
   // Optional: Add these two lines to slow I2C clock to 12.5kHz from 100 kHz
   // This is best for long wire lengths to minimize errors
@@ -45,24 +65,23 @@ void setup() {
 }
 
 //Check motor connection, return false if any motors fail
-boolean getMotorStatus(){
+void getMotorStatus(){
   boolean stat = true, fail = false;
   for(int i = 0; i < numMotors; i++){
-    stat = motors[i].isAlive();
-    if(stat = false)
-      fail = false;
-    motorCon[i] = stat;
+    motorStatusMsg.data = motors[i].isAlive();
+    pubMotorStatus.publish(&motorStatusMsg);
   }
-  return fail;
 }
 
 void getMotorRPM(){
-  for(int i = 0; i < numMotors; i++)
-    motorRPM[i] = (int)motors[i].rpm();
+  for(int i = 0; i < numMotors; i++){
+    motorRPMMsg.data = (int)motors[i].rpm();
+    pubMotorRPM.publish(&motorRPMMsg); 
+  }
 }
 
 //Run actual update function that updates values from motor controllers
-//Also call functions to store these values
+//Also call functions to report these values
 void motorUpdate(){
   for(int i = 0; i < numMotors; i++)
     motors[i].update();
@@ -72,9 +91,9 @@ void motorUpdate(){
 }
 
 //This function converts a value between -1 and 1 to a throttle value
-//Actual throttle values are-32767 to 32767
+//Actual throttle values are -32767 to 32767
 int16_t percentToThrottle(float t){
-  return (int16_t)(t * 32767);
+  return (int16_t)(t * 32767) * maxThrust;
 }
 
 //Command motors to run at values specified in motorCommand array
@@ -83,73 +102,22 @@ void setMotorThrottles(){
     motors[i].set(percentToThrottle(motorCommand[i]));
 }
 
-//{ForwardPort,ForwardAft,DepthFore,DepthAft,StrafeTop,StrafeBottom,
-// Torpedo1,Torpedo2,Dropper1,Dropper2,Arm}
-void parseSerialInput(){
-  
-}
+void sensorUpdate(){
+  int hydrophone_sensor_pin1= analogRead(hydroPin1);
+  int hydrophone_sensor_pin2= analogRead(hydroPin2);
+  int hydrophone_sensor_pin3= analogRead(hydroPin3);
 
-//Make sure serial input string is properly formatted
-//String should be list of X numbers separated by commas, and wrapped in {}
-boolean validInput(String s){
-  if(s.charAt(0) != '{' || s.charAt(s.length() - 1) != '{'){
-    //Error output
-    return false;
-  }
-  int currentIndex = 1;
-  int numNumbers;//Number of integers separated by commas in string
-  for(int i = 1; i < s.length(); i++){
-    if(s.charAt(i) == ',' || i == s.length() - 2){
-      for(int j = 0; j < i - currentIndex; j++){
-        if(!isDigit(s.charAt(j)))
-          return false;
-      }
-      numNumbers++;
-    }
-  }
-  
-  if(numNumbers != 11)
-    return false;
-  
-  return true;
-}
+  //Calculate hydrophone heading and distance
+  hydroMsg = ???; 
+  pubHydro.publish(&hydroMsg);
 
-String buildSerialOutput(){
-  
+  depthMsg = analogRead(depthPin);
+  pubDepth.publish(&depthMsg);
 }
-
-String packaging_sensor_data(int pressure_sensor_count, 
-                             int hydrophone_sensor_pin1, 
-                             int hydrophone_sensor_pin2, 
-                             int hydrophone_sensor_pin3 )
-{
-  String temp ;
-  temp.concat("P:");temp.concat(pressure_sensor_count); 
-  temp.concat("H1:");temp.concat(hydrophone_sensor_pin1);
-  temp.concat("H2:");temp.concat(hydrophone_sensor_pin2);
-  temp.concat("H3:");temp.concat(hydrophone_sensor_pin3);
-  return temp;
-  //Serial.println(temp);  
-}
-
 
 void loop() {
-  
-  int pressure_sensor_count = analogRead(0);
-  int hydrophone_sensor_pin1= analogRead(1);
-  int hydrophone_sensor_pin2= analogRead(2);
-  int hydrophone_sensor_pin3= analogRead(3);
-  String  sensor_data_package = packaging_sensor_data( pressure_sensor_count, 
-                              hydrophone_sensor_pin1, 
-                              hydrophone_sensor_pin2, 
-                              hydrophone_sensor_pin3 );
- 
-  if ( Serial.available() > 0 ) {
-    signal = Serial.parseInt();
-  }
-//Serial.write(sensor_data_package);
   motorUpdate();
-  delay(1000); // Update at roughly 4 hz for the demo
-  
-
+  sensorUpdate();
+  nh.spinOnce();
+  delay(1000); // Update at roughly 4 hz
 }
