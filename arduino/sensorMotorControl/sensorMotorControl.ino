f5cf66a9502bac054337a00ce21b6d3e328a3e25
Jake <jacobjenks@gmail.com>
2016-05-19 14:56:14 -0600

    Created message types for peripheral communications, and finished Arduino control script. It should (theoretically) only require minor debugging at this point.

--- arduino/sensorMotorControl/sensorMotorControl.ino
+++ arduino/sensorMotorControl/sensorMotorControl.ino
@@ -2,20 +2,25 @@
 #include "Arduino_I2C_ESC.h"
 #include <ctype.h>
 #include <ros.h>
+#include <msurobosub/MotorStatus.h>
+#include <msurobosub/Hydro.h>
+#include <std_msgs/Float32.h>
+#include <msurobosub/MotorCommand.h>
+#include <msurobosub/PneumaticCommand.h>
 
 
-int signal;
 const int numMotors = 6;
-String serialMessage;
+const int hydroPin1 = A1;
+const int hydroPin2 = A2;
+const int hydroPin3 = A3;
+const int depthPin = A4;
 
 ros::NodeHandle nh;
-std_msgs:: motorStatusMsg;
-ros::Publisher pubMotorStatus("motor_status", &motorStatusMsg);
-std_msgs:: motorRPMMsg;
-ros::Publisher pubMotorRpm("motor_rpm", &motorRPMMsgs);
+msurobosub::MotorStatus motorStatusMsg;
+ros::Publisher pubMotorStatus("motorStatus", &motorStatusMsg);
 std_msgs::Float32 depthMsg;
 ros::Publisher pubDepth("depth", &depthMsg);
-std_msgs:: hydroMsg;
+msurobosub::Hydro hydroMsg;
 ros::Publisher pubHydro("hydrophone", &hdryMsg)
 
 enum MotorE{
@@ -36,58 +41,47 @@ Arduino_I2C_ESC motors[numMotors] = {
   Arduino_I2C_ESC(0x2B)//StrafeBottom
 };
 
-boolean motorCon[numMotors];//Motor connection status
-int motorRPM[numMotors];//Motor RPM
-//Array of % thrust values we'd like each motor to operate at
-//Values range between -1 and 1 for 100% forward/reverse thrust
-float motorCommand[] = {0,0,0,0,0,0};
 float maxThrust = .75;//Percent value indicating what power level we consider to be max thrust
 
-void motorCommandCallback(const std_msgs::Empty& command){
-//route command to motor
-//msg type needs to be something other than Empty, but not sure what it should be yet
-//msg should contain motor ID and thrust setting
+void motorCommandCallback(const msurobosub::MotorCommand& command){
+  motors[command.motor_id] = command.power;
 }
 
-ros::Subscriber<std_msgs::Empty> commandSub("motorCommand", &motorCommandCB);
+void pneumaticCommandCallback(const msurobosub::PneumaticCommand& command){
+
+}
+
+ros::Subscriber<msurobosub::MotorCommand> subMotorCommand("motorCommand", &motorCommandCallback);
+ros::Subscriber<msurobosub::PneumaticCommand> subPneumaticCommand("pneumaticCommand", &pneumaticCommandCallback);
 
 void setup() {
   Wire.begin();
   nh.initNode();
-  nh.advertise(depth);
+  nh.advertise(pubMotorStatus);
+  nh.advertise(pubHydro);
+  nh.advertise(pubDepth);
+  nh.subscribe(subMotorCommand);
+  nh.subscribe(subPneumaticCommand);
 
   // Optional: Add these two lines to slow I2C clock to 12.5kHz from 100 kHz
   // This is best for long wire lengths to minimize errors
   //TWBR = 158;  
   //TWSR |= bit (TWPS0);
-  
-  getMotorStatus();
 }
 
-//Check motor connection, return false if any motors fail
-void getMotorStatus(){
-  boolean stat = true, fail = false;
-  for(int i = 0; i < numMotors; i++){
-    motorStatusMsg.data = motors[i].isAlive();
-    pubMotorStatus.publish(&motorStatusMsg);
-  }
-}
-
-void getMotorRPM(){
-  for(int i = 0; i < numMotors; i++){
-    motorRPMMsg.data = (int)motors[i].rpm();
-    pubMotorRPM.publish(&motorRPMMsg); 
-  }
-}
 
 //Run actual update function that updates values from motor controllers
-//Also call functions to report these values
 void motorUpdate(){
-  for(int i = 0; i < numMotors; i++)
+  for(int i = 0; i < numMotors; i++){
     motors[i].update();
-
-  getMotorStatus();
-  getMotorRPM();
+    motorStatusMsg.motor_id = i;
+    motorStatusMsg.status = motors[i].isAlive() ? 1 : 0;
+    motorStatusMsg.rpm = motors[i].rpm();
+    motorStatusMsg.voltage = motors[i].voltage();
+    motorStatusMsg.current = motors[i].current();
+    motorStatusMsg.temperature = motors[i].temperature();
+    pubMotorStatus.publish(&motorStatusMsg); 
+  }
 }
 
 //This function converts a value between -1 and 1 to a throttle value
@@ -96,22 +90,17 @@ int16_t percentToThrottle(float t){
   return (int16_t)(t * 32767) * maxThrust;
 }
 
-//Command motors to run at values specified in motorCommand array
-void setMotorThrottles(){
-  for(int i = 0; i < numMotors; i++)
-    motors[i].set(percentToThrottle(motorCommand[i]));
-}
-
 void sensorUpdate(){
-  int hydrophone_sensor_pin1= analogRead(hydroPin1);
-  int hydrophone_sensor_pin2= analogRead(hydroPin2);
-  int hydrophone_sensor_pin3= analogRead(hydroPin3);
+  int hydrophone_sensor_pin1 = analogRead(hydroPin1);
+  int hydrophone_sensor_pin2 = analogRead(hydroPin2);
+  int hydrophone_sensor_pin3 = analogRead(hydroPin3);
 
   //Calculate hydrophone heading and distance
-  hydroMsg = ???; 
+  hydroMsg.distance = 0; 
+  hydroMsg.degree = 0;
   pubHydro.publish(&hydroMsg);
 
-  depthMsg = analogRead(depthPin);
+  depthMsg.data = analogRead(depthPin);
   pubDepth.publish(&depthMsg);
 }
 
