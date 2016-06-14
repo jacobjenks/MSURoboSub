Realized I was seriously inflating imu variance values, and corrected them. Localization is better, but still drifts without fusing other sensors.

msurobosub_ros/src/msurobosub/sensorVariance.py
@@ -14,14 +14,14 @@ def saveVariance():
 	global sensorReadings
 	f = open('/home/robosub/MSURoboSub/msurobosub_ros/src/msurobosub/calibration/sensor_variance.txt', 'w')
 	for key in sorted(sensorReadings):
-		f.write("%s variance: %.3f\n" % (key, np.var(sensorReadings[key])))
-		f.write("%s average: %.3f\n" % (key, np.average(sensorReadings[key])))
-		f.write("%s min: %.3f\n" % (key, np.min(sensorReadings[key])))
-		f.write("%s max: %.3f\n" % (key, np.max(sensorReadings[key])))
-		print "%s variance: %.3f" % (key, np.var(sensorReadings[key]))
-		print "%s average: %.3f" % (key, np.average(sensorReadings[key]))
-		print "%s min: %.3f" % (key, np.min(sensorReadings[key]))
-		print "%s max: %.3f" % (key, np.max(sensorReadings[key]))
+		f.write("%s variance: %.5f\n" % (key, np.var(sensorReadings[key])))
+		f.write("%s average: %.5f\n" % (key, np.average(sensorReadings[key])))
+		f.write("%s min: %.5f\n" % (key, np.min(sensorReadings[key])))
+		f.write("%s max: %.5f\n" % (key, np.max(sensorReadings[key])))
+		print "%s variance: %.5f" % (key, np.var(sensorReadings[key]))
+		print "%s average: %.5f" % (key, np.average(sensorReadings[key]))
+		print "%s min: %.5f" % (key, np.min(sensorReadings[key]))
+		print "%s max: %.5f" % (key, np.max(sensorReadings[key]))
 
 
 #Recursively dig through sensor message and record all readings