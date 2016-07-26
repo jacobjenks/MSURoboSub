291c32f3967f9714877a77c2f7846234e67afee2
Jake <jacobjenks@gmail.com>
2016-07-25 21:31:02 -0700

    Added rosbag to launch file, and added pneumatics/depth sensor to arduino

--- msurobosub_ros/src/msurobosub/arduino_motors/arduino_motors.ino
+++ msurobosub_ros/src/msurobosub/arduino_motors/arduino_motors.ino
@@ -1,3 +1,9 @@
+/*
+ * This arduino relays motor commands from ROS to the motors.
+ * This script still contains code for reading motor status info
+ * using BlueRobotics I2C library. However, none of it is in use, since
+ * we switched to using PWM due to most of our BlueESCs blowing up.
+*/
 #include <Wire.h>
 #include <Servo.h>
 #include <ctype.h>
@@ -9,16 +15,25 @@
 #include <msurobosub/PneumaticCommand.h>
 #include <std_msgs/Header.h>
 
-/*
- * This arduino relays motor commands from ROS to the motors.
- * This script still contains code for reading motor status info
- * using BlueRobotics I2C library. However, none of it is in use, since
- * we switched to using PWM due to most of our BlueESCs blowing up.
-*/
+const int torp1Pin = 2;
+const int torp2Pin = 3;
+const int drop1Pin = 4;
+const int drop2Pin = 5;
+const int armOpenPin = 6;
+const int armClosePin = 7;
+const int depthPin = 0;
+
+bool pneumaticLock = true;
+float surfacePSI = -1;
+
+int pneumaticShutoffPin = 0;//Pin to shut off
+unsigned long pneumaticShutoffTime = 0;//Time at which we should turn off pneumatic valve
 
 const int numMotors = 6;
 
 ros::NodeHandle nh;
+msurobosub::Depth depthMsg;
+ros::Publisher pubDepth("sensors/depth", &depthMsg);
 
 /*
 Arduino_I2C_ESC motors[numMotors] = {
@@ -62,7 +77,83 @@ void motorCommandCallback(const msurobosub::MotorCommand& command){
   lastMotorCommand[5] = percentToThrottle(command.power[5], 5);
 }
 
+void pneumaticCommandCallback(const msurobosub::PneumaticCommand& command){
+  switch(command.command){
+    case 0://Pneumatic lock
+      if(pneumaticLock){
+        pneumaticLock = false;
+        nh.loginfo("Pneumatics unlocked");
+      }
+      else{
+        pneumaticLock = true;
+        nh.loginfo("Pneumatics locked");
+      }
+      break;
+    case 1://Torp 1
+      if(!pneumaticLock){
+        nh.loginfo("Firing torpedo 1");
+        activatePneumatics(torp1Pin, 250);
+      }
+      break;
+    case 2://Torp 2
+      if(!pneumaticLock){
+        nh.loginfo("Firing torpedo 2");
+        activatePneumatics(torp2Pin, 250);
+      }
+      break;
+    case 3://Drop 1
+      if(!pneumaticLock){
+        nh.loginfo("Releasing dropper 1");
+        activatePneumatics(drop1Pin, 250);
+      }
+      break;
+    case 4://Drop 2
+      if(!pneumaticLock){
+        nh.loginfo("Releasing dropper 1");
+        activatePneumatics(drop2Pin, 250);
+      }
+      break;
+    case 5://Arm open
+      if(!pneumaticLock){
+        nh.loginfo("Opening arm");
+        activatePneumatics(armOpenPin, 3000);
+      }
+      break;
+    case 6://Arm close
+      if(!pneumaticLock){
+        nh.loginfo("Closing arm");
+        activatePneumatics(armClosePin, 3000);
+      }
+      break;
+    default:
+      nh.logerror("Invalid pneumatics command");
+  }
+}
+
 ros::Subscriber<msurobosub::MotorCommand> subMotorCommand("command/motor", &motorCommandCallback);
+ros::Subscriber<msurobosub::PneumaticCommand> subPneumaticCommand("command/pneumatic", &pneumaticCommandCallback);
+
+void activatePneumatics(int pin, int duration){
+  digitalWrite(pin, LOW);
+  pneumaticShutoffTime = millis() + duration;
+  pneumaticShutoffPin = pin;
+}
+
+std_msgs::Header getHeader(){
+  std_msgs::Header updated = std_msgs::Header();
+  updated.seq = 0;
+  updated.stamp = nh.now();
+  updated.frame_id = "0";
+  return updated;
+}
+
+std_msgs::Header getHeader(std_msgs::Header h){
+  std_msgs::Header updated = std_msgs::Header();
+  updated.seq = h.seq + 1;
+  updated.stamp = nh.now();
+  updated.frame_id = h.frame_id;
+  return updated;
+}
 
 //Update motor readings/power setting
 void motorUpdate(){
@@ -104,10 +195,43 @@ int percentToThrottle(float t, int motor){
   return int(1500 + t*400*maxThrust*direction[motor]);//PWM
 }
 
+void sensorUpdate(){
+  depthMsg.header = getHeader(depthMsg.header);
+  //Convert depth sensor reading to PSI
+  depthMsg.psi = (analogRead(depthPin) * .0048828125 - 1)*12.5;
+  
+  if(surfacePSI == -1)
+    surfacePSI = depthMsg.psi;
+
+  //Convert difference between surface and current PSI to depth in meters.
+  depthMsg.depth = ((analogRead(depthPin) * .0048828125 - 1)*12.5 - surfacePSI)*.13197839577;
+  pubDepth.publish(&depthMsg);
+}
+
 void setup() {
   Wire.begin();
   nh.initNode();
   nh.subscribe(subMotorCommand);
+  nh.advertise(pubDepth);
+  nh.subscribe(subPneumaticCommand);
+
+  //Initialize pneumatics
+  pinMode(torp1Pin, OUTPUT);
+  pinMode(torp2Pin, OUTPUT);
+  pinMode(drop1Pin, OUTPUT);
+  pinMode(drop2Pin, OUTPUT);
+  pinMode(armOpenPin, OUTPUT);
+  pinMode(armClosePin, OUTPUT);
+  digitalWrite(armOpenPin, HIGH);
+  digitalWrite(armClosePin, HIGH);
+  digitalWrite(torp1Pin, HIGH);
+  digitalWrite(torp2Pin, HIGH);
+  digitalWrite(drop1Pin, HIGH);
+  digitalWrite(drop2Pin, HIGH);
+
+  //Initialize messages
+  depthMsg.header = getHeader();
+  depthMsg.header.frame_id = "depth";
 
   for(int i = 0; i < numMotors; i++){
     motors[i].attach(motorPins[i]);
@@ -117,6 +241,14 @@ void setup() {
 }
 
 void loop() {
+  sensorUpdate();
+
+  //check for pneumatics shutoff
+  if(pneumaticShutoffTime != 0 && millis() > pneumaticShutoffTime){
+    digitalWrite(pneumaticShutoffPin, HIGH);
+    pneumaticShutoffTime = 0;
+  }
+  
   motorUpdate();
   nh.spinOnce();
 }
